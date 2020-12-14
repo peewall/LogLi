2e47b67629b1b00bfcb1e0ff74b2706aa2080349
Peter Wallace <peter.wallace@quicken.com>
2020-12-13 21:11:27 -0600

    initial project commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import clsx from "clsx";
+import {mainTheme} from "./Themes/mainTheme";
+// ======= MUI IMPORTS =======
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {makeStyles} from '@material-ui/core/styles';
+import {Typography} from '@material-ui/core';
+import AppDrawer from "./Components/AppDrawer";
+// ==========
 import './App.css';
 
+const useStyles = makeStyles((mainTheme) => ({
+
+    mainContent: {
+        margin: '4em',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '4em',
+    }
+}));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <CssBaseline/> {/* Used to normalize styles to MUI across the app */}
+            <AppDrawer/>
+            <main className={classes.mainContent}>
+                <Typography h1>
+                    This page is under development.
+                    Check back soon for logli updates.
+                </Typography>
+            </main>
+
+        </React.Fragment>
+    );
 }
 
 export default App;
